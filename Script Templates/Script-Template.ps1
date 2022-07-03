340079b37620d481a60c1cdd2088a6b48f307ddb
raysanborn1 <raysanborn1@gmail.com>
2022-07-03 10:12:35 -0400

    Added the first batch of files

---
+++ Script Templates/Script-Template.ps1
@@ -0,0 +1,57 @@
+﻿<#
+
+.SYNOPSIS
+
+Provide a basic description of what the PowerShell Script does. Think end goal, not every function in its entirety.
+
+.DESCRIPTION
+
+Provide a more detailed description here including what each function does, required modules, etc.
+
+.EXAMPLE
+
+Provide examples here. EACH example should be separated by '.EXAMPLE'
+
+.EXAMPLE
+
+Provide examples here. EACH example should be separated by '.EXAMPLE'
+
+.NOTES
+
+Created by: Your Name Here
+Email Address: first.last@jax.org
+Date of Creation: mm/dd/yyyy
+
+Date of update: mm/dd/yyyy
+Updated by: Name of Updater here
+
+#>
+
+##############################
+#### PowerShell Modules ######
+##############################
+
+
+##############################
+### PowerShell Functions #####
+##############################
+
+
+##############################
+### User Defined Variables ###
+##############################
+
+
+############################################
+####### Pre-Script Command Execution #######
+############################################
+
+
+##############################
+## Script Defined Variables ##
+##############################
+
+
+##############################
+### Script Execution #########
+##############################
